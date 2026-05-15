c185426c6399b9fbef2755f57425aa70f83829d9
Quevedo <lucas.qvdo@gmail.com>
2026-05-15 13:20:17 -0300

    arquivo vite_config renomeado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './EstiloHome.css';
+import './Home.css';
 
 function Home() {
   const [activeSlide, setActiveSlide] = useState(0);
